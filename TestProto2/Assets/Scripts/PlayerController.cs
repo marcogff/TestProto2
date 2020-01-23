41d3b254574a20b94248e30cca7bead66f2aa654
Marco <marco.graffi15@gmail.com>
2020-01-23 18:06:37 -0300

    Interactable part 1

--- TestProto2/Assets/Scripts/PlayerController.cs
+++ TestProto2/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float speed = 5f;
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+    transform.position = transform.position + new Vector3 (horizontalInput * speed * Time.deltaTime, 0, verticalInput * speed * Time.deltaTime);
+
+    Debug.Log (transform.position);
+
+    if (Input.GetKeyDown(KeyCode.E))
+    {
+        // sphere gizmo interact w quad to open chest w colliders
+        // Debug.Log ($"<b> Chest Open </b>");
     }
+
+    }
+
+    // void Awake()
+    // {
+    //     transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+
+    //     GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+    //     Camera.main.transform.position = new Vector3(0.85f, 1.0f, -3.0f);
+
+    //     target = cylinder.transform;
+    //     target.transform.localScale = new Vector3(0.15f, 1.0f, 0.15f);
+    //     target.transform.position = new Vector3(0.8f, 0.0f, 0.8f);
+
+    //     Camera.main.transform.position = new Vector3(0.85f, 1.0f, -3.0f);
+    //     Camera.main.transform.localEulerAngles = new Vector3(15.0f, -20.0f, -0.5f);
+
+    //     // Create and position the floor.
+    //     GameObject floor = GameObject.CreatePrimitive(PrimitiveType.Plane);
+    //     floor.transform.position = new Vector3(0.0f, -1.0f, 0.0f);
+    // }
+    
+
+    // void Update()
+    // {
+    //     float step = speed * Time.deltaTime;
+    //     transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    // }
 }
