8e6259dff2ab611ff51b92935879631049bfe898
Mariano <infernuzigt@gmail.com>
2020-01-25 17:49:55 -0300

    Interaction

---
+++ TestProto2/Assets/Scripts/Interaction.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+
+public class Interaction : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // Turn on box collider 
+        // Character can interact
+        Debug.Log ($"<b> Interaction </b>");
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        // Turn off box collider 
+        // Character can't interact
+        Debug.Log ($"<b> No Interaction </b>");
+    }
+}
