6035d1f56ea6d324c1ed64c5cb053c8eae4a4c4a
Mariano <infernuzigt@gmail.com>
2020-01-25 17:08:44 -0300

    Movement

--- TestProto2/Assets/Scripts/PlayerController.cs
+++ TestProto2/Assets/Scripts/PlayerController.cs
@@ -4,47 +4,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public bool isSmooth;
+    [Space]
     public float speed = 5f;
-    void Update()
+    public float speedSmooth = 10f;
+
+    private float _moveHorizontal;
+    private float _moveVertical;
+
+    private float _posX;
+    private float _posZ;
+
+    private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-    transform.position = transform.position + new Vector3 (horizontalInput * speed * Time.deltaTime, 0, verticalInput * speed * Time.deltaTime);
-
-    Debug.Log (transform.position);
-
-    if (Input.GetKeyDown(KeyCode.E))
-    {
-        // sphere gizmo interact w quad to open chest w colliders
-        // Debug.Log ($"<b> Chest Open </b>");
+        Movement();
+        Interaction();
     }
 
+    private void Movement()
+    {
+        if (isSmooth)
+        {
+            _moveHorizontal = Input.GetAxis("Horizontal");
+            _moveVertical = Input.GetAxis("Vertical");
+
+            _posX = _moveHorizontal * speedSmooth * Time.deltaTime;
+            _posZ = _moveVertical * speedSmooth * Time.deltaTime;
+        }
+        else
+        {
+            _moveHorizontal = Input.GetAxisRaw("Horizontal");
+            _moveVertical = Input.GetAxisRaw("Vertical");
+
+            _posX = _moveHorizontal * speed * Time.deltaTime;
+            _posZ = _moveVertical * speed * Time.deltaTime;
+        }
+
+        transform.position += new Vector3(_posX, 0, _posZ);
     }
 
-    // void Awake()
-    // {
-    //     transform.position = new Vector3(0.0f, 0.0f, 0.0f);
-
-    //     GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-    //     Camera.main.transform.position = new Vector3(0.85f, 1.0f, -3.0f);
-
-    //     target = cylinder.transform;
-    //     target.transform.localScale = new Vector3(0.15f, 1.0f, 0.15f);
-    //     target.transform.position = new Vector3(0.8f, 0.0f, 0.8f);
-
-    //     Camera.main.transform.position = new Vector3(0.85f, 1.0f, -3.0f);
-    //     Camera.main.transform.localEulerAngles = new Vector3(15.0f, -20.0f, -0.5f);
-
-    //     // Create and position the floor.
-    //     GameObject floor = GameObject.CreatePrimitive(PrimitiveType.Plane);
-    //     floor.transform.position = new Vector3(0.0f, -1.0f, 0.0f);
-    // }
-    
-
-    // void Update()
-    // {
-    //     float step = speed * Time.deltaTime;
-    //     transform.position = Vector3.MoveTowards(transform.position, target.position, step);
-    // }
+    private void Interaction()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            // sphere gizmo interact w quad to open chest w colliders
+            // Debug.Log ($"<b> Chest Open </b>");
+        }
+    }
 }
